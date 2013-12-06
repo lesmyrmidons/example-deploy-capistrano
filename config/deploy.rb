a49dad32a54cda94dc987f8b092c2e4afd4db73d
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-12-06 11:43:31 +0100

    Add files for deploy with capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+set :application, 'cap-sfr'
+set :repo_url, 'https://github.com/lesmyrmidons/example-deploy-capistrano.git'
+set :branch,   'master'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/www/#{application}'
+set :scm, :git
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Start Server'
+  task :started do
+    execute ''
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
